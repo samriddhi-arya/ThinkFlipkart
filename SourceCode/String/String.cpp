46ae1b6689e6e0c952c5d0c7f3a111b18e7376a2
samriddhi-arya <43422407+samriddhi-arya@users.noreply.github.com>
2020-04-30 14:21:59 +0530

    Added toUpper function toString.cpp

--- SourceCode/String/String.cpp
+++ SourceCode/String/String.cpp
@@ -4,6 +4,14 @@ void greetings(char hello[]){
 	printf("\n%s",hello);
 }
 
+void toUpper(char string[]){
+	printf("Making String UPPER...");
+}
+
+void toLower(char string[]){
+	printf("Making String lower...");
+}
+
 int main(){
 	int greet[] = "Hello World";
 
